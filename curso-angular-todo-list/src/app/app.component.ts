af711ccd65b2ea93f3fe884b39b92c5aaa71eb87
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-03-15 15:16:25 -0300

    feat: create List component

--- curso-angular-todo-list/src/app/app.component.ts
+++ curso-angular-todo-list/src/app/app.component.ts
@@ -5,7 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<router-outlet />`
+  template: `
+  <p>teste</p>
+  <router-outlet />
+  `
 })
 export class AppComponent {
 
